84e1205d6a87884c93cd84adc11a1686877c6f02
Davide Pizzolotto <davide.pizzolotto@studenti.unitn.it>
2017-12-11 19:13:01 +0100

    Static method test

---
+++ java2c/src/test/java/it/se/obfuscator/tests/TestAdderStaticSum.java
@@ -0,0 +1,37 @@
+package it.se.obfuscator.tests;
+
+import it.se.obfuscator.helpers.TestMethodTemplate;
+
+public class TestAdderStaticSum extends TestMethodTemplate
+{
+
+    @Override
+    public String getTestClassName()
+    {
+        return testclasses.AdderStaticMethod.class.getName();
+    }
+
+    @Override
+    public String getMethodName()
+    {
+        return "add";
+    }
+
+    @Override
+    public Class[] getMethodParamsSignature()
+    {
+        return new Class[]{int.class,int.class};
+    }
+
+    @Override
+    public Object[] getMethodParams()
+    {
+        return new Object[]{15,21};
+    }
+
+    @Override
+    public Object getMethodExpectedResult()
+    {
+        return 36;
+    }
+}
