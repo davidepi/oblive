f7920b1b2efd14e8c14ca0e7b5955281fa8adc05
Davide Pizzolotto <davide.pizzolotto@studenti.unitn.it>
2018-03-06 19:01:09 +0100

    ILoad tests

---
+++ java2c/src/test/java/testclasses/stack/ILoad.java
@@ -0,0 +1,17 @@
+package testclasses.stack;
+
+import it.se.obfuscator.Obfuscate;
+
+public class ILoad
+{
+    public ILoad()
+    {
+
+    }
+
+    @Obfuscate
+    public int exec(int a)
+    {
+        return a;
+    }
+}
