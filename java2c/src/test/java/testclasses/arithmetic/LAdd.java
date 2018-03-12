9559b3d95641d4980b181e8c9b14826f0b19265b
Davide Pizzolotto <davide.pizzolotto@studenti.unitn.it>
2018-03-12 13:08:09 +0100

    Addition (I,L,F,D) + tests

---
+++ java2c/src/test/java/testclasses/arithmetic/LAdd.java
@@ -0,0 +1,18 @@
+package testclasses.arithmetic; //when changing this, remember to change also the variable in it.se.obfuscator.helpers
+
+import it.se.obfuscator.Obfuscate;
+
+public class LAdd
+{
+    public LAdd()
+    {
+
+    }
+
+    @Obfuscate
+    public long add(long a, long b)
+    {
+        return a + b;
+    }
+
+}
